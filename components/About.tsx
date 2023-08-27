14474d396658ef964e3f293a71e1997ae72e6a50
Faizan Ali <fa1694701@gmail.com>
2023-08-27 04:28:59 -0700

    new changing

--- components/About.tsx
+++ components/About.tsx
@@ -1,23 +1,60 @@
 const About = () => {
     return (
         <div className="d-flex justify-content-center">
-            <div className="card" style={{width: "55rem",borderRadius:"0px"}}>
-                
-                    <div className="card-body" style={{padding:"50px"}}>
-                        <h2 style={{fontFamily:"Quicksand,sans-serif",letterSpacing:".08em",fontSize:"30px",fontWeight:"700"}}>ABOUT ME</h2>
-                        <p className="pt-3" style={{fontFamily:"Quicksand,sans-serif",letterSpacing:".08em",fontWeight:"700",fontSize:"16px"}}>Hello, I’m Faizan Ali!</p>
-                        <p style={{fontFamily:"Quicksand,sans-serif",letterSpacing:".08em"}}>Self-motivated and self-taught Software Engineer, passionate about collaborating with great people.</p>
-                        <p className="pt-3" style={{fontFamily:"Quicksand,sans-serif",letterSpacing:".08em"}}>Over the course of six months in the industry, I have honed my abilities through hands-on experience and collaboration with talented teams. I possess a keen eye for detail, which allows me to write efficient, maintainable code that adheres to best practices.</p>
-                        <p className="pt-3" style={{fontFamily:"Quicksand,sans-serif",letterSpacing:".08em"}}>Feel free to reach out to me to explore my skills and experience further, or to discuss collaboration on an exciting project.</p>
-                       <div className="pt-2">
-                       <hr />
+            <div className="card" style={{ width: "55rem", borderRadius: "0px" }}>
 
-                       </div>
-                       <h2 style={{fontFamily:"Quicksand,sans-serif",letterSpacing:".08em",fontSize:"30px",fontWeight:"700"}}>ABOUT ME</h2>
-                        
-                       
-                        
+                <div className="card-body" style={{ padding: "50px" }}>
+                    <h2 style={{ fontFamily: "Bahnschrift Light", letterSpacing: ".08em", fontSize: "30px", fontWeight: "700" }}>ABOUT ME</h2>
+                    <p className="pt-3" style={{ fontFamily: "Bahnschrift Light", letterSpacing: ".08em", fontWeight: "700", fontSize: "16px" }}>Hello, I’m Faizan Ali!</p>
+                    <p style={{ fontFamily: "Bahnschrift Light", letterSpacing: ".08em" }}>Self-motivated and self-taught Software Engineer, passionate about collaborating with great people.</p>
+                    <p className="pt-3" style={{ fontFamily: "Bahnschrift Light", letterSpacing: ".08em" }}>Over the course of six months in the industry, I have honed my abilities through hands-on experience and collaboration with talented teams. I possess a keen eye for detail, which allows me to write efficient, maintainable code that adheres to best practices.</p>
+                    <p className="pt-3" style={{ fontFamily: "Bahnschrift Light", letterSpacing: ".08em" }}>Feel free to reach out to me to explore my skills and experience further, or to discuss collaboration on an exciting project.</p>
+                    <div className="pt-2">
+                        <hr />
+
                     </div>
+                    <h2 className="pt-3" style={{ fontFamily: "Bahnschrift Light", letterSpacing: ".08em", fontSize: "30px", fontWeight: "700" }}>SKILLS</h2>
+                    <p className="pt-4 mb-0" style={{ fontFamily: "Bahnschrift Light", letterSpacing: ".08em", fontSize: "20px", fontWeight: "200" }}>PRIMARY AREA OF EXPERTISE</p>
+                    <div className="row">
+                        <div className="col-lg-4 col-md-6 col-sm-12 col-xs-12 pt-2"><p  style={{ fontFamily: "Bahnschrift Light", letterSpacing: ".08em", fontWeight: "700", fontSize: "15px",marginBottom:"0px" }}>React</p>
+                        <span className="pt-2" style={{ display: "inline-flex", alignItems: "center" }}>
+                            <span className="bullet fill" style={{backgroundColor:"#c0e3e7",marginRight:"6px",display: "inline-block",borderColor:"#c0e3e7",width:"0.938em",height:"0.938em",borderRadius:"50%",borderWidth:"1px",borderStyle:"solid",whiteSpace:"nowrap"}}></span>   
+                            <span className="bullet fill" style={{backgroundColor:"#c0e3e7",marginRight:"6px",display: "inline-block",borderColor:"#c0e3e7",width:"0.938em",height:"0.938em",borderRadius:"50%",borderWidth:"1px",borderStyle:"solid",whiteSpace:"nowrap"}}></span>                         
+                            <span className="bullet fill" style={{backgroundColor:"#c0e3e7",marginRight:"6px",display: "inline-block",borderColor:"#c0e3e7",width:"0.938em",height:"0.938em",borderRadius:"50%",borderWidth:"1px",borderStyle:"solid",whiteSpace:"nowrap"}}></span>                         
+                            <span className="bullet fill" style={{backgroundColor:"#c0e3e7",marginRight:"6px",display: "inline-block",borderColor:"#c0e3e7",width:"0.938em",height:"0.938em",borderRadius:"50%",borderWidth:"1px",borderStyle:"solid",whiteSpace:"nowrap"}}></span>                         
+                            <span className="bullet fill" style={{marginRight:"6px",display: "inline-block",borderColor:"#c0e3e7",width:"0.938em",height:"0.938em",borderRadius:"50%",borderWidth:"1px",borderStyle:"solid",whiteSpace:"nowrap"}}></span>                         
+                            <span className="bullet fill" style={{marginRight:"6px",display: "inline-block",borderColor:"#c0e3e7",width:"0.938em",height:"0.938em",borderRadius:"50%",borderWidth:"1px",borderStyle:"solid",whiteSpace:"nowrap"}}></span>                         
+
+                        </span>
+                        </div>
+                        <div className="col-lg-4 col-md-6 col-sm-12 col-xs-12 pt-2"><p  style={{ fontFamily: "Bahnschrift Light", letterSpacing: ".08em", fontWeight: "700", fontSize: "15px",marginBottom:"0px" }}>Next JS</p>
+                        <span className="pt-2" style={{ display: "inline-flex", alignItems: "center" }}>
+                            <span className="bullet fill" style={{backgroundColor:"#c0e3e7",marginRight:"6px",display: "inline-block",borderColor:"#c0e3e7",width:"0.938em",height:"0.938em",borderRadius:"50%",borderWidth:"1px",borderStyle:"solid",whiteSpace:"nowrap"}}></span>   
+                            <span className="bullet fill" style={{backgroundColor:"#c0e3e7",marginRight:"6px",display: "inline-block",borderColor:"#c0e3e7",width:"0.938em",height:"0.938em",borderRadius:"50%",borderWidth:"1px",borderStyle:"solid",whiteSpace:"nowrap"}}></span>                         
+                            <span className="bullet fill" style={{backgroundColor:"#c0e3e7",marginRight:"6px",display: "inline-block",borderColor:"#c0e3e7",width:"0.938em",height:"0.938em",borderRadius:"50%",borderWidth:"1px",borderStyle:"solid",whiteSpace:"nowrap"}}></span>                         
+                            <span className="bullet fill" style={{backgroundColor:"#c0e3e7",marginRight:"6px",display: "inline-block",borderColor:"#c0e3e7",width:"0.938em",height:"0.938em",borderRadius:"50%",borderWidth:"1px",borderStyle:"solid",whiteSpace:"nowrap"}}></span>                         
+                            <span className="bullet fill" style={{backgroundColor:"#c0e3e7",marginRight:"6px",display: "inline-block",borderColor:"#c0e3e7",width:"0.938em",height:"0.938em",borderRadius:"50%",borderWidth:"1px",borderStyle:"solid",whiteSpace:"nowrap"}}></span>                         
+                            <span className="bullet fill" style={{marginRight:"6px",display: "inline-block",borderColor:"#c0e3e7",width:"0.938em",height:"0.938em",borderRadius:"50%",borderWidth:"1px",borderStyle:"solid",whiteSpace:"nowrap"}}></span>                         
+
+                        </span>
+                        </div>
+                        <div className="col-lg-4 col-md-6 col-sm-12 col-xs-12 pt-2"><p  style={{ fontFamily: "Bahnschrift Light", letterSpacing: ".08em", fontWeight: "700", fontSize: "15px",marginBottom:"0px" }}>Gatsby</p>
+                        <span className="pt-2" style={{ display: "inline-flex", alignItems: "center" }}>
+                            <span className="bullet fill" style={{backgroundColor:"#c0e3e7",marginRight:"6px",display: "inline-block",borderColor:"#c0e3e7",width:"0.938em",height:"0.938em",borderRadius:"50%",borderWidth:"1px",borderStyle:"solid",whiteSpace:"nowrap"}}></span>   
+                            <span className="bullet fill" style={{backgroundColor:"#c0e3e7",marginRight:"6px",display: "inline-block",borderColor:"#c0e3e7",width:"0.938em",height:"0.938em",borderRadius:"50%",borderWidth:"1px",borderStyle:"solid",whiteSpace:"nowrap"}}></span>                         
+                            <span className="bullet fill" style={{backgroundColor:"#c0e3e7",marginRight:"6px",display: "inline-block",borderColor:"#c0e3e7",width:"0.938em",height:"0.938em",borderRadius:"50%",borderWidth:"1px",borderStyle:"solid",whiteSpace:"nowrap"}}></span>                         
+                            <span className="bullet fill" style={{backgroundColor:"#c0e3e7",marginRight:"6px",display: "inline-block",borderColor:"#c0e3e7",width:"0.938em",height:"0.938em",borderRadius:"50%",borderWidth:"1px",borderStyle:"solid",whiteSpace:"nowrap"}}></span>                         
+                            <span className="bullet fill" style={{backgroundColor:"#c0e3e7",marginRight:"6px",display: "inline-block",borderColor:"#c0e3e7",width:"0.938em",height:"0.938em",borderRadius:"50%",borderWidth:"1px",borderStyle:"solid",whiteSpace:"nowrap"}}></span>                         
+                            <span className="bullet fill" style={{marginRight:"6px",display: "inline-block",borderColor:"#c0e3e7",width:"0.938em",height:"0.938em",borderRadius:"50%",borderWidth:"1px",borderStyle:"solid",whiteSpace:"nowrap"}}></span>                         
+
+                        </span>
+                        </div>
+
+                    </div>
+
+
+
+                </div>
             </div>
 
 
